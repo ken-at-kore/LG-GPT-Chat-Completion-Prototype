2327f90fe360d3a97effa875d3ef079af81e1f5a
ken-at-kore <146371853+ken-at-kore@users.noreply.github.com>
2023-10-20 10:22:19 -0400

    Implemented Convo Memory w /compression

--- AiBot.py
+++ AiBot.py
@@ -1,5 +1,6 @@
 import typing
 from typing import List
+import traceback
 import streamlit as st
 from datetime import datetime
 import openai
@@ -50,6 +51,9 @@ class StreamlitAiBot:
         # Initialize AiFunctions
         ai_funcs_arg = kwargs.get('ai_functions', None)
         self.ai_functions = AiFunction.Collection(ai_funcs_arg) if ai_funcs_arg is not None else AiFunction.Collection()
+
+        # Initialize Conversation Memory
+        self.convo_memory = StreamlitAiBot.ConvoMemory(self.system_prompt_engineering, self.welcome_message)
 
         # Initialize chain of thought config
         self.do_cot = True
@@ -114,16 +118,9 @@ class StreamlitAiBot:
         if "openai_model" not in st.session_state:
             st.session_state["openai_model"] = self.openai_model
 
-        # Set up the GPT system prompt with the current date
-        system_promt = self.system_prompt_engineering + '\n\nCurrent date: ' + datetime.now().strftime('%Y-%m-%d')
-
-        # Initialize UI messages and GPT messages
+        # Initialize UI messages
         if "messages" not in st.session_state:
             st.session_state.messages = [
-                {"role": "assistant", "content": self.welcome_message}
-            ]
-            st.session_state["gpt_messages"] = [
-                {"role": "system", "content": system_promt},
                 {"role": "assistant", "content": self.welcome_message}
             ]
 
@@ -133,12 +130,13 @@ class StreamlitAiBot:
                 st.markdown(message["content"])
 
         # Get, store and render user message
-        if prompt := st.chat_input("Enter text here"):
-            print(f"AiBot: User input: {prompt}")
-            st.session_state.messages.append({"role": "user", "content": prompt})
-            st.session_state["gpt_messages"].append({"role": "user", "content": prompt.replace('$','\$')})
+        if user_input := st.chat_input("Enter text here"):
+            print(f"AiBot: User input: {user_input}")
+            user_input = user_input.replace('$','\$') # Try to sanitize against LaTeX markup
+            self.convo_memory.add_user_msg(user_input) # Add user input to conversation memory
+            st.session_state.messages.append({"role": "user", "content": user_input}) # Add it to the UI thread
             with st.chat_message("user"):
-                st.markdown(prompt.replace('$','\$')) # Try to avoid LaTeX markup
+                st.markdown(user_input) # Render it
 
             # Initialize processing counters
             self.function_error_count = 0
@@ -147,24 +145,27 @@ class StreamlitAiBot:
             # Call GPT with the input and process results
             self.call_and_process_gpt()
 
+            # Consider compressing conversation memory
+            self.convo_memory.maybe_compress_memory()
+
         else:
             print("AiBot: Didn't process input.")
 
 
     def call_and_process_gpt(self):
         """
-        Use the chat history in st.session_state['gpt_messages'] to call ChatGPT.
+        Use the chat history in Conversation Memory to call ChatGPT.
         Process the bot message and function call results.
         """
 
         # Keep track of how often call & processing happens this turn
-        self.call_and_process_count = self.call_and_process_count + 1
+        self.call_and_process_count += 1
 
-        # Prepare GPT messages (conversation context). Include chain of thought logging for improved decision making.
-        convo_context = st.session_state["gpt_messages"]
+        # Prepare GPT messages (conversation context). Include Chain of Thought logging for improved decision making.
+        convo_context = self.convo_memory.get_messages()
         if self.do_cot:
-            cot_str = self.do_chain_of_thought_logging()
-            convo_context = convo_context + [{"role": "function", "name": "do_chain_of_thought_logging", "content": cot_str}]
+            cot_result = self.generate_chain_of_thought_logging()
+            convo_context = convo_context + [{"role": "function", "name": "do_chain_of_thought_logging", "content": cot_result}]
 
         # Prepare assistant response UI
         function_call_name = ""
@@ -216,16 +217,17 @@ class StreamlitAiBot:
         if not function_call_response:
             print(f"AiBot: GPT assistant message: {bot_content_response}")
             # Store the bot content
-            st.session_state['gpt_messages'].append({"role": "assistant", "content": bot_content_response})
+            self.convo_memory.add_assistant_msg(bot_content_response)
             st.session_state.messages.append({"role": "assistant", "content": bot_content_response})
 
         # Handle function call
         else:
             print(f'AiBot: Got GPT function call "{function_call_name}". Content: "{bot_content_response}". Arguments: {function_call_response}')
+
             # Store bot content including function call name and arguments
-            st.session_state['gpt_messages'].append({"role": "assistant", "content": bot_content_response,
-                                                        "function_call": {"name": function_call_name, 
-                                                                        "arguments": function_call_response}})
+            self.convo_memory.add_assistant_msg(bot_content_response, function_call_name, function_call_response)
+
+            # Add bot content to UI thread
             if bot_content_response != "":
                 st.session_state.messages.append({"role": "assistant", "content": bot_content_response})
 
@@ -237,7 +239,7 @@ class StreamlitAiBot:
                 assert isinstance(func_call_results, AiFunction.Result), f"func_call_results for {function_call_name} must be of type AiFunction.Result, not {type(func_call_results)}"
             except Exception as e:
                 print(f"AiBot: Caught function calling error {function_call_name}.\n{e}")
-                self.function_error_count = self.function_error_count + 1
+                self.function_error_count += 1
                 func_call_results = AiFunction.Result(str(e))
                 # raise e
 
@@ -245,15 +247,18 @@ class StreamlitAiBot:
 
             # Store query results for GPT
             print(f"AiBot: Function execution result: {func_call_results_str}")
-            st.session_state['gpt_messages'].append({"role": "function", "name": function_call_name, 
-                                                     "content": func_call_results_str})
+            self.convo_memory.add_function_msg(function_call_name, func_call_results_str)
 
             # Recursively call this same function to process the query results
             self.call_and_process_gpt()
 
 
 
-    def do_chain_of_thought_logging(self):
+    def generate_chain_of_thought_logging(self) -> str:
+        """"
+        Implements the Chain of Thought technique by having the bot explain the context and then the next step.
+        This generated CoT content is then fed into the next GPT call.
+        """
 
         cot_func_spec = {
             "name": "do_chain_of_thought_logging",
@@ -273,18 +278,84 @@ class StreamlitAiBot:
                 "required": ["describe_the_context", "describe_what_you_should_do_next"],
             }
         }
+        messages = self.convo_memory.get_messages()
         funcs = self.ai_functions.get_function_specs() + [cot_func_spec]
         print("AiBot: Calling GPT for chain of thought...")
-        response = openai.ChatCompletion.create(
-            model=self.openai_model,
-            messages=st.session_state["gpt_messages"],
-            temperature=self.model_temperature,
-            functions=funcs,
-            function_call={"name": "do_chain_of_thought_logging"}
-        )
+        try:
+            response = openai.ChatCompletion.create(
+                model=self.openai_model,
+                messages=messages,
+                temperature=self.model_temperature,
+                functions=funcs,
+                function_call={"name": "do_chain_of_thought_logging"}
+            )
+        except Exception as e:
+            print("Caught error when doing chain of thought.")
+            traceback.print_exc()
         response = json.loads(response.choices[0].message.function_call.arguments)
         cot_result = f"{response['describe_the_context']} | {response['describe_what_you_should_do_next']}"
         print(f"AiBot: Chain of thought: {cot_result}")
         return cot_result
         
 
+
+    class ConvoMemory:
+        """
+        This encapsulates the conversation memory and context. Beside keeping track of the running messages list,
+        this memory generates conversation summaries to reduce tokens sent to the GPT context window.
+        """
+
+        def __init__(self, system_prompt_engineering:str = "You are a helpful assistant.",
+                     bot_welcome_message:str = None):
+            self.system_prompt_engineering = system_prompt_engineering # The primary content for the system message
+            self.convo_summary = None # The conversatoin summary that will be appended to the system message content
+            self.interaction_messages = [] # The messages excluding the first system message
+            if bot_welcome_message is not None:
+                self.interaction_messages.append({'role':'assistant', 'content':bot_welcome_message})
+
+        def add_user_msg(self, content:str):
+            self.interaction_messages.append({'role':'user', 'content':content})
+
+        def add_assistant_msg(self, content:str, function_call_name:str=None, function_call_arguments:str=None):
+            msg = {'role':'assistant', 'content':content}
+            if function_call_name is not None or function_call_arguments is not None:
+                assert function_call_name is not None and function_call_arguments is not None, "Both function_call_name and function_call_arguments need to be set."
+                msg['function_call'] = {'name': function_call_name, 'arguments': function_call_arguments}
+            self.interaction_messages.append(msg)
+
+        def add_function_msg(self, function_name:str, content:str):
+            self.interaction_messages.append({'role':'function', 'name':function_name, 'content':content})
+
+        def get_messages(self):
+            result_system_content = self.system_prompt_engineering
+            result_system_content += '\n\nCurrent date: ' + datetime.now().strftime('%Y-%m-%d')
+            if self.convo_summary is not None:
+                result_system_content += "\n\n---\n\nCONVERSATION SUMMARY\n\n" + self.convo_summary
+            return [{'role':'system', 'content':result_system_content}] + self.interaction_messages
+        
+        def maybe_compress_memory(self):
+            #TODO: Move numbers to configuration
+            memory_messages_capacity_threshold = 8
+            target_msg_count_to_summarize = 4
+            if len(self.interaction_messages) > memory_messages_capacity_threshold:
+                print("AiBot: Compressing memory...")
+                target_messages = self.interaction_messages[:target_msg_count_to_summarize] #TODO: Get the older messages
+                compression_system_prompt = 'You are an expert conversation designer and a helpful assistant.'
+                compression_user_prompt = 'Summarize the following GPT conversation. Be specific and include facts. ' \
+                                            'But you must write less than 150 words.\n\n'
+                if self.convo_summary is not None:
+                    compression_user_prompt += f'Include this partial summary info in your summary: {self.convo_summary}\n\nNew part of conversation: '
+                compression_user_prompt += json.dumps(target_messages, indent=2)
+                try:
+                    response = openai.ChatCompletion.create(
+                        model='gpt-3.5-turbo', #TODO: Ability to configure this
+                        messages=[{'role':'system','content':compression_system_prompt},
+                                  {'role':'user','content':compression_user_prompt}],
+                        temperature=0, #TODO: Ability to configure this
+                    )
+                    self.convo_summary = response.choices[0].message.content
+                    self.interaction_messages = self.interaction_messages[target_msg_count_to_summarize:] # Remove old non-system messages from memory
+                    print(f"AiBot: Got summary: {self.convo_summary}")
+                except Exception as e:
+                    print("Caught error when doing memory compression.")
+                    traceback.print_exc()
--- my_streamlit_aibot.py
+++ my_streamlit_aibot.py
@@ -8,7 +8,7 @@ def run():
         StreamlitAiBot.initialize(streamlit_page_title='LG AiBot Prototype',
                             openai_model='gpt-3.5-turbo',
                             model_temperature=0.25,
-                            system_prompt_engineering=open('prompts & content/welcome message.md').read(),
+                            system_prompt_engineering=open('prompts & content/system prompt.md').read(),
                             welcome_message=open('prompts & content/welcome message.md').read(),
                             ai_functions=[SearchForLGProducts()]
         )
